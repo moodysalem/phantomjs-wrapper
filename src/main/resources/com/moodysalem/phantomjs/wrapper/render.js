a8bf90f2def395c7d4fb04266a521e3578d8abe6
Moody Salem <moody@fastmodeltechnologies.com>
2016-03-18 09:18:50 -0500

    only do render after the page finishes loading

--- src/main/resources/com/moodysalem/phantomjs/wrapper/render.js
+++ src/main/resources/com/moodysalem/phantomjs/wrapper/render.js
@@ -63,6 +63,34 @@ var paperSize = {
 
 page.paperSize = paperSize;
 
+page.onResourceRequested = function (requestData, request) {
+  log('loading url: ', requestData[ 'url' ]);
+};
+
+page.onLoadFinished = function () {
+  if (operatingSystem !== "WINDOWS") {
+    try {
+      log('setting zoom on html to 0.75');
+      page.evaluate(function () {
+        document.body.style.zoom = 0.75;
+      });
+    } catch (error) {
+      err('failed to set zoom on html file: ' + error);
+      phantom.exit(2);
+    }
+  }
+
+  try {
+    log('rendering to target path: ' + targetPath);
+    page.render(targetPath);
+  } catch (error) {
+    err('failed to render pdf: ' + error);
+    phantom.exit(3);
+  }
+
+  phantom.exit(0);
+};
+
 try {
   log('reading source file: ' + sourcePath);
   page.content = fs.read(sourcePath, { encoding: 'utf-8' });
@@ -70,25 +98,3 @@ try {
   err('failed to read source file: ' + error);
   phantom.exit(1);
 }
-
-if (operatingSystem !== "WINDOWS") {
-  try {
-    log('setting zoom on html to 0.75');
-    page.evaluate(function () {
-      document.body.style.zoom = 0.75;
-    });
-  } catch (error) {
-    err('failed to set zoom on html file: ' + error);
-    phantom.exit(2);
-  }
-}
-
-try {
-  log('rendering to target path: ' + targetPath);
-  page.render(targetPath);
-} catch (error) {
-  err('failed to render pdf: ' + error);
-  phantom.exit(3);
-}
-
-phantom.exit(0);
