567764ec3f899173c0915c63d38d715ee8f7f957
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2016-08-21 12:50:32 -0500

    Some pom updates

--- exec/src/main/java/com/moodysalem/java/Render.java
+++ exec/src/main/java/com/moodysalem/java/Render.java
@@ -18,25 +18,24 @@ import java.nio.file.Paths;
  * A command line tool for accessing the render method of the phantomJS binary
  */
 public class Render {
+    /**
+     * Render the first argument to a PDF located at the second argument
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        try (
+                final InputStream html = Files.newInputStream(Paths.get(args[0]));
+                final InputStream pdf = PhantomJS.render(null, html, PaperSize.Letter, ViewportDimensions.VIEW_1280_1024,
+                        Margin.ZERO, BannerInfo.EMPTY, BannerInfo.EMPTY, RenderFormat.PDF, 10000L, 100L);
+        ) {
 
-	/**
-	 * Render the html in argument 1 to argument 2
-	 */
-	public static void main(String[] args) {
-		try (
-				InputStream html = Files.newInputStream(Paths.get(args[0]));
-				InputStream pdf = PhantomJS.render(null, html, PaperSize.Letter, ViewportDimensions.VIEW_1280_1024, 
-						Margin.ZERO, BannerInfo.EMPTY, BannerInfo.EMPTY, RenderFormat.PDF, 10000L, 100L);
-			){
+            Path dest = Paths.get(args[1]);
+            Files.deleteIfExists(dest);
+            Files.copy(pdf, dest);
 
-			Path dest = Paths.get(args[1]);
-			Files.deleteIfExists(dest);
-			Files.copy(pdf, dest);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (RenderException e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (IOException | RenderException e) {
+            e.printStackTrace();
+        }
+    }
 }
